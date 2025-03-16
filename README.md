a48657238d287ff5234f8efc3d2474cc348ce258
JANISA18 <janicesunzu@gmail.com>
2025-03-16 17:42:40 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Janice-Portfolio
+Showcase my skills, Provide my work history, academic history, and contact details.
+use the index.html file to begin viewing the website on google chrome
---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+<<<<<<< HEAD
+/*!
+
+*/
+
+
+
+
+
+=======
+>>>>>>> fc1480c (Initial commit or latest updates)
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slides');
+const slideInterval = 8000; // 8 seconds per slide
+const fadeDuration = 1500; // Matches CSS transition duration (1.5 seconds)
+
+function showSlides() {
+    slides.forEach((slide, i) => {
+    slide.classList.remove('active');
+    if (i === slideIndex) {
+        slide.classList.add('active');
+    }
+    });
+    slideIndex = (slideIndex + 1) % slides.length; // Move to the next slide
+    setTimeout(showSlides, slideInterval - fadeDuration); // Overlap transition time
+}
+
+function plusSlides(n) {
+    slideIndex = (slideIndex + n - 1 + slides.length) % slides.length;
+    // slideIndex = (slideIndex + n + slides.length) % slides.length;
+    showSlides();
+}
+
+// Start the slideshow
+showSlides();
+
